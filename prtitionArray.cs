474fdf915713d0b8e4ca78c56a99e055992bb2c4
Mohammadreza Bonyadi <118369462+Mohammadreza0083@users.noreply.github.com>
2024-02-04 00:08:16 +0330

    Patition array class method max sum

---
+++ prtitionArray.cs
@@ -0,0 +1,24 @@
+﻿namespace MaxSumAfterPartitioning
+{
+    internal class prtitionArray : IMaxSumAfterPartitioning
+    {
+        public int? Maxsum(int[] arr, int partitionLenght)
+        {
+            int arrayLenght = arr.Length;
+            int[] dp = new int[arrayLenght + 1];
+
+            for (int i = 1; i <= arrayLenght; i++)
+            {
+                int maxVal = 0;
+                for (int j = 1; j <= partitionLenght && i - j >= 0; j++)
+                {
+                    maxVal = Math.Max(maxVal, arr[i - j]);
+                    dp[i] = Math.Max(dp[i], dp[i - j] + maxVal * j);
+                }
+            }
+            int maxSumAfterPartion = dp[arrayLenght];
+            return maxSumAfterPartion ;
+            
+        }
+    }
+}
